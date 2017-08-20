f10df8ec4f9ac9752b04bc3e7ffef3ed39bdc5bc
Andranik <anakinouki@hotmail.fr>
2017-08-20 19:14:11 -0400

    loading

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController} from 'ionic-angular';
+import { NavController, LoadingController} from 'ionic-angular';
 import { Observable } from 'rxjs/Observable';
 import { ApiProvider } from './../../providers/api/api';
 
@@ -10,16 +10,40 @@ import { ApiProvider } from './../../providers/api/api';
 export class HomePage {
   USER: Observable<any>;
   userMoney: number;
-  
+
   posts: Observable<any>;
 
-  constructor(public navCtrl: NavController, public apiProvider: ApiProvider) {
+  constructor(public navCtrl: NavController, public apiProvider: ApiProvider, public loadingCtrl: LoadingController) {
     this.USER = this.apiProvider.getUser();
     this.USER.subscribe(data => {
       this.userMoney = data[0].money;
     });
-
     this.posts = this.apiProvider.getFeedPosts();
   }
 
+  doRefresh(refresher) {
+    console.log('Begin async operation', refresher);
+
+    setTimeout(() => {
+      console.log('Async operation has ended');
+      refresher.complete();
+    }, 2000);
+  }
+
+  presentLoadingCustom() {
+    let loading = this.loadingCtrl.create({
+      spinner: 'dots',
+      content: `
+        <div class="custom-spinner-container">
+          <div class="custom-spinner-box"></div>
+        </div>`,
+      duration: 3000
+    });
+
+    loading.onDidDismiss(() => {
+      console.log('Dismissed loading');
+    });
+
+    loading.present();
+  }
 }
